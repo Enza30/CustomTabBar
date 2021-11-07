3a32b53060acf97172d8b6f4600908fb387f779a
Farendza <farendza.muda@gmail.com>
2021-11-08 03:40:01 +0700

    Custom Tab Bar

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -8,9 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedIndex = 0
+    @State var shouldShowModel = false
+    
+    let tabBarImageNames = ["person","gear","plus.app.fill", "pencil", "lasso"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0){
+            ZStack{
+                
+                Spacer()
+                    .fullScreenCover(isPresented: $shouldShowModel, content: {
+                        Button(action: {shouldShowModel.toggle()}, label: {
+                            Text("Fullscreen cover")
+                        })
+                        
+                    })
+                
+                switch selectedIndex {
+                case 0:
+                    NavigationView{
+                        ScrollView{
+                            ForEach(0..<50) { num in
+                                Text("\(num)")
+                                
+                            }
+                        }
+                            .navigationTitle("First Tab")
+                    }
+                    
+                case 1:
+                    ScrollView{
+                        Text("Test")
+                    }
+                    
+                default:
+                    NavigationView{
+                        Text("Remaining tabs")
+                    }
+                   
+                }
+                
+                
+            }
+            
+//            Spacer()
+            
+            Divider()
+                .padding(.bottom, 8)
+            
+            HStack{
+                ForEach(0..<5) { num in
+                    Button(action: {
+                        
+                        if num == 2 {
+                            shouldShowModel.toggle()
+                            return
+                        }
+                        
+                        selectedIndex = num
+                    }, label: {
+                        Spacer()
+                        
+                        if num == 2 {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 44, weight: .bold))
+                                .foregroundColor(.red)
+                        } else {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+                        }
+                        
+                        
+                        Spacer()
+                    })
+
+                    
+                }
+            }
+        }
     }
 }
 
