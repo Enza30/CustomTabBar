1c75fa3814e175653112e20fd01672ebd92e0c1d
Farendza <farendza.muda@gmail.com>
2021-11-08 01:55:00 +0700

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Farendza Muda on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
